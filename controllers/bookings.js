7b167c3f6611a6469eb999bfb51b3303dc0a5fce
Your Name <you@example.com>
2022-12-01 18:06:27 +0100

    Fixed database problems

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -1,18 +1,18 @@
 import db from "../database"
 
 const bookingsController = {
- find: async (id) => {
-   const booking =await db.User.findByPk(id)
-   return JSON.parse(JSON.stringify(booking))
- },
- findAll: async () => {
-   const bookings = await db.User.findAll()
-   return JSON.parse(JSON.stringify(bookings))
- },
- create: async (data) => {
-   const booking = await db.User.create(data)
-   return JSON.parse(JSON.stringify(booking))
- }
+  find: async (id) => {
+    const booking = await db.Booking.findByPk(id)
+    return JSON.parse(JSON.stringify(booking))
+  },
+  findAll: async () => {
+    const bookings = await db.Booking.findAll()
+    return JSON.parse(JSON.stringify(bookings))
+  },
+  create: async (data) => {
+    const booking = await db.Booking.create(data)
+    return JSON.parse(JSON.stringify(booking))
+  }
 }
 
 export default bookingsController;
--- pages/flats/[id]/bookings/index.js
+++ pages/flats/[id]/bookings/index.js
@@ -1,10 +1,25 @@
-
+import bookingsController from "../../../../controllers/bookings"
 export default function Profile(props) {
+    const bookings = props.bookings
+    console.log(bookings)
     return (
         <>
             <h1>List of trips</h1>
+            <ul>
+                <li>Rome, IT</li>
+
+            </ul>
         </>
     )
 
 }
 
+export async function getServerSideProps(req, res) {
+    const id = req.query.id
+    const bookings = await bookingsController.findAll()
+    return {
+
+        props: { bookings },
+        // will be passed to the page component as props
+    }
+}
