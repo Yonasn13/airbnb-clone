1ee89ea5ffacda2bf9e7c7c2b2c6846478ef49b4
yonase <yonasnuguse1234@gmail.com>
2022-12-02 17:35:04 +0100

    check

--- pages/flats/[id]/index.js
+++ pages/flats/[id]/index.js
@@ -1,11 +1,40 @@
-import db from '../../../database'
-import flatsController from '../../../controllers/flatsController'
+import userController from "../controllers/flatsController"
 
-export default function Details(props) {
+export default function coach(props) {
+    const flats = props.flats
+    console.log(flats)
     return (
         <>
-            <h1>Flat Details</h1>
+            <div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur.<br />
+                    Lacus porttitor ac a cras viverra nascetur tincidunt et lectus.<br />
+                    Elementum ac dui pellentesque purus.
+                </p>
+                {flats.map(flat => (
+                    <p key={flat.id} >Address: {flat.address} <br />
+                        Price: {flat.price}</p>
+                ))}
+            </div>
+            <div>
+                <form action="/bookings/new" method="POST">
+                    <input hidden name="flatId" value={flats.id} />
+                    <label htmlFor="startDate">Start Date</label>
+                    <input type='date' id="startDate" name="startDate" />
+                    <label htmlFor="endDate">End Date</label>
+                    <input type='date' id="endDate" name="endDate" />
+                    <input type="submit" value="REQUEST BOOKING" onClick="submit" />
+                </form>
+            </div>
         </>
     )
+}
+
+export async function getServerSideProps(req, res) {
+    const flats = await userController.all()
+    return {
+        props: { flats },
+        // will be passed to the page component as props
+    }
 }
 
