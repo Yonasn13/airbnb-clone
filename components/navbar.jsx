486dec6848d09a3de363712c83f05acff8b78ae4
yonase <yonasnuguse1234@gmail.com>
2022-12-02 12:00:40 +0100

    navbar adjacted

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -4,10 +4,16 @@ import styles from "../styles/Navbar.module.css";
 export default function Component(props) {
     // fix the routes so that they send to the correct path
     return (
-        <nav className={styles.navbar}>
-            <div className={styles.navlink}>
-                <Link href={`/`}>Home Page</Link>
-            </div>
-        </nav>
+        
+        <>
+          <nav className={styles.navbar}>
+              <div className={styles.navlink}>
+                  <Link href={`/`}>Home Page</Link>
+              </div>
+              <div className={styles.navlink}>
+              <Link href={'/profile'}>Profile</Link>
+              </div>
+          </nav>
+        </>
     )
 }
